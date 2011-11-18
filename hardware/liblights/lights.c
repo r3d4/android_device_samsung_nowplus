2f3375137e5a1376a655bb958d44b99bfbc2bbbf
r3d4 <terrae@gmx.de>
2011-11-18 12:10:32 +0100

    add option tho show battery charging on LED with settin light.battery.show=1 in system.prop/build.prop add bluetooth patch
    Change-Id: I6b707fdfeafbb9ace74048cdbf556d7a27ebd06c

--- hardware/liblights/lights.c
+++ hardware/liblights/lights.c
@@ -30,13 +30,19 @@
 #include <sys/types.h>
 
 #include <hardware/lights.h>
+#include <cutils/properties.h>
+
 
 /******************************************************************************/
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
+static struct light_state_t g_notification;
+static struct light_state_t g_battery;
 static int g_backlight = 255;
 static int g_buttons = 0;
+static int g_show_charging = 0;
+
 struct led_prop {
     const char *filename;
 };
@@ -50,23 +56,32 @@ struct led {
 };
 
 enum {
-    AMBER_LED,
+    BOTTOM_LED,
     LCD_BACKLIGHT,
     NUM_LEDS,
 };
 
 struct led leds[NUM_LEDS] = {
-    [AMBER_LED] = {
+    [BOTTOM_LED] = {
         .brightness = { "/sys/class/leds/led1_B/brightness"},
         .trigger = { "/sys/class/leds/led1_B/trigger"},
         .delay_on = { "/sys/class/leds/led1_B/delay_on"},
-        .delay_off = { "/sys/class/leds/led1_B/delay_off"},        
+        .delay_off = { "/sys/class/leds/led1_B/delay_off"},
     },
     [LCD_BACKLIGHT] = {
         .brightness = { "/sys/class/leds/lcd-backlight/brightness"},
     },
 };
 
+struct light_config {
+    int brightness;
+    int delay_on;
+    int delay_off;
+};
+// define LED behaviour for notification and charging
+//                                            /brightness,  on delay,   off delay
+struct light_config LED_CFG_NOTIFICATION    = {255,         500,        2000};
+struct light_config LED_CFG_CHARGE          = {255,         250,        10};
 
 /**
  * device methods
@@ -86,14 +101,14 @@ write_string(struct led_prop *prop, const char *value)
 
     if (!prop->filename)
         return 0;
-        
+
     int fd = open(prop->filename, O_RDWR);
     if (fd < 0) {
         LOGE("init_prop: %s cannot be opened (%s)\n", prop->filename,
              strerror(errno));
         return -errno;
     }
-  
+
     if (fd < 0)
         return 0;
 
@@ -110,7 +125,7 @@ write_string(struct led_prop *prop, const char *value)
         bytes -= amt;
     }
 
-success:        
+success:
     return 0;
 failed:
     if (fd > 0)
@@ -150,9 +165,9 @@ set_light_backlight(struct light_device_t* dev,
     //LOGD("%s brightness=%d color=0x%08x",__func__,brightness, state->color);
     pthread_mutex_lock(&g_lock);
     g_backlight = brightness;
-    
+
     err = write_int(&leds[LCD_BACKLIGHT].brightness, brightness);
-	
+
     pthread_mutex_unlock(&g_lock);
     return err;
 }
@@ -168,20 +183,68 @@ static int
 set_light_buttons(struct light_device_t* dev,
         struct light_state_t const* state)
 {
-        return 0;    
+    return 0;
 }
+
+
+static int set_led_light_locked(struct light_state_t const* state, struct light_config *led_config )
+{
+    struct led *bottom_led = &leds[BOTTOM_LED];
+    unsigned int colorRGB = state->color & 0xFFFFFF;
+        
+    LOGV("%s: led=%s mode=%d color=0x%08x On=%d Off=%d\n", __func__, 
+        state==&g_battery?"battery":"notification",
+        state->flashMode, state->color, state->flashOnMS, state->flashOffMS);
+
+    switch (state->flashMode) {
+    case LIGHT_FLASH_TIMED:
+        write_string(&bottom_led->trigger, "timer");      
+        write_int(&bottom_led->delay_on, led_config->delay_on);
+        write_int(&bottom_led->delay_off, led_config->delay_off);
+        //falltrough
+    case LIGHT_FLASH_HARDWARE:
+    case LIGHT_FLASH_NONE:      //alway enable light if color is set
+        if (colorRGB == 0) {
+            write_int(&bottom_led->brightness, 0);
+        }
+        else {
+            write_int(&bottom_led->brightness, led_config->brightness);
+
+        }
+        break;
+
+    default:
+        LOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
+        state->color, state->flashMode);
+    }
+    return 0;
+}
+
+
+
+static void
+handle_led_battery_locked(struct light_device_t* dev)
+{
+    if (is_lit(&g_battery)) {   //charging has priority over notification
+        set_led_light_locked( &g_battery, &LED_CFG_CHARGE);
+    } else {
+        set_led_light_locked( &g_notification, &LED_CFG_NOTIFICATION);
+    }
+}
+
 
 static int
 set_light_battery(struct light_device_t* dev,
         struct light_state_t const* state)
 {
-#if 0
-    pthread_mutex_lock(&g_lock);
-    LOGV("%s mode=%d color=0x%08x",
-            __func__,state->flashMode, state->color);
-    set_amber_light_locked(dev, state);
-    pthread_mutex_unlock(&g_lock);
-#endif
+    if (g_show_charging)
+    {
+        pthread_mutex_lock(&g_lock);
+        g_battery = *state;
+        handle_led_battery_locked(dev);
+        pthread_mutex_unlock(&g_lock);
+    }
+
     return 0;
 }
 
@@ -190,34 +253,10 @@ set_light_notifications(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
-
-    unsigned int colorRGB = state->color & 0xFFFFFF;
-    LOGV("%s mode=%d color=0x%08x On=%d Off=%d\n", __func__,state->flashMode, state->color, state->flashOnMS, state->flashOffMS);
-    
-        switch (state->flashMode) {
-        case LIGHT_FLASH_HARDWARE:
-        case LIGHT_FLASH_TIMED:
-            if (colorRGB == 0) {
-                write_int(&leds[AMBER_LED].brightness, 0);
-            }
-            else {
-                write_int(&leds[AMBER_LED].brightness, 255);
-                write_string(&leds[AMBER_LED].trigger, "timer");
-                write_int(&leds[AMBER_LED].delay_on, 500);
-                write_int(&leds[AMBER_LED].delay_off, 2000);
-            }
-
-            break;
-        case LIGHT_FLASH_NONE:
-            //LOGV("set_led_state colorRGB=%08X, on\n", state->color);
-            write_int(&leds[AMBER_LED].brightness, 0);
-            break;
-        default:
-            LOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
-                  state->color, state->flashMode);
-    }
-
+    g_notification = *state;
+    handle_led_battery_locked(dev);
     pthread_mutex_unlock(&g_lock);
+
     return 0;
 }
 
@@ -225,35 +264,8 @@ static int
 set_light_attention(struct light_device_t* dev,
         struct light_state_t const* state)
 {
-    
-    LOGV("%s color=0x%08x mode=0x%08x", __func__, state->color, state->flashMode);
+	/* nowplus has no attention, bad nowplus */
 
-    pthread_mutex_lock(&g_lock);
-    
-        unsigned int colorRGB = state->color & 0xFFFFFF;
-        switch (state->flashMode) {
-        case LIGHT_FLASH_HARDWARE:
-        case LIGHT_FLASH_TIMED:
-            if (colorRGB == 0) {
-                write_int(&leds[AMBER_LED].brightness, 0);
-            }
-            else {
-                write_int(&leds[AMBER_LED].brightness, 255);
-                write_string(&leds[AMBER_LED].trigger, "timer");
-                write_int(&leds[AMBER_LED].delay_on, 250);
-                write_int(&leds[AMBER_LED].delay_off, 10);
-            }
-
-            break;
-        case LIGHT_FLASH_NONE:
-            write_int(&leds[AMBER_LED].brightness, 0);
-            break;
-        default:
-            LOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
-                  state->color, state->flashMode);
-    }
-
-    pthread_mutex_unlock(&g_lock);
     return 0;
 }
 
@@ -280,6 +292,7 @@ close_lights(struct light_device_t *dev)
 static int open_lights(const struct hw_module_t* module, char const* name,
         struct hw_device_t** device)
 {
+    char value[PROPERTY_VALUE_MAX];
     int (*set_light)(struct light_device_t* dev,
             struct light_state_t const* state);
 
@@ -305,11 +318,14 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         return -EINVAL;
     }
 
+    property_get("light.battery.show", value, "");
+    if (value[0] == '1') g_show_charging = 1;
+  
     pthread_once(&g_init, init_globals);
 
     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
     memset(dev, 0, sizeof(*dev));
-
+   
     dev->common.tag = HARDWARE_DEVICE_TAG;
     dev->common.version = 0;
     dev->common.module = (struct hw_module_t*)module;
